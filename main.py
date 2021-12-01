bd6d47a3c7d9d7bb40606e134c3e9ed851cb0449
Gino Vincenzini <OpenMySourceCode@gmail.com>
2021-11-30 23:53:33 -0500

    Updated main file with new methods and called display example

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ from SendEmail import Email, TXTHandler
 from Templates import EmailTemplate
 from minify_html import minify
 
+def save_csv(group):
+    # create a csv file of the results:
+    # name, full-name, email-address, amazon-link, who-assigned-to
+    pass
+
+def display_example(person, template, txtTemplate):
+    # create template with person
+    # show example on terminal of template
+    # open web browser of example for html
+    pass
+
 def main():
     group = PollyannaGroup('data.csv')
     group.shuffle()
@@ -11,11 +22,15 @@ def main():
     # If they're correct, save them in a text file in the user's dir
     # Then continue on, otherwise quit.
     
+    
     template = EmailTemplate('Template', 'template.html')
     txtTemplate = EmailTemplate('Template', 'txttemplate.txt', False)
     
-    # Grab the first person in the group, and open the template in a browser
-    # Also print the text template. 
+    for person in group:
+        displayExample(person, template, txtTemplate)
+        break
+    
+    # Prompt to continue or quit
     
     for person in group:
         render = template.render_for_person(person)
